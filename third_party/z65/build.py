aa543bba8014003ff2adcc3ddb3fb3f216363125
Henrik Löfgren <henrik.lofgren@gmail.com>
2026-01-26 08:01:59 +0100

    Added DEBUG and STATUS as build flags

--- third_party/z65/build.py
+++ third_party/z65/build.py
@@ -4,7 +4,7 @@ from tools.build import unixtocpm
 
 llvmprogram(
     name="z65",
-    cflags=["-Ithird_party/zmalloc"],
+    cflags=["-Ithird_party/zmalloc -DDEBUG=0 -DSTATUS=1"],
     srcs=["./z65.c"],
     deps=["lib+cpm65", "third_party/zmalloc+zmalloc"],
 )
